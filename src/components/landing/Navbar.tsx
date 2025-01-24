4a4576000b3664edfde617c3183b6480fd61a937
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2025-01-24 12:48:45 +0530

    chore: removed unused sections and Theme toggle

--- src/components/landing/Navbar.tsx
+++ src/components/landing/Navbar.tsx
@@ -17,29 +17,12 @@ const Navbar = () => {
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 <a
-                  href="#hero"
-                  className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Home
-                </a>
-                <a
                   href="#features"
                   className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Features
                 </a>
-                <a
-                  href="#demoCanvas"
-                  className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Demo
-                </a>
-                <a
-                  href="#tutorials"
-                  className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Tutorials
-                </a>
+
                 <a
                   href="#pricing"
                   className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
@@ -95,28 +78,10 @@ const Navbar = () => {
       >
         <div className="px-2 pt-2 pb-3 space-y-1">
           <a
-            href="#hero"
-            className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Home
-          </a>
-          <a
             href="#features"
             className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
           >
             Features
-          </a>
-          <a
-            href="#demoCanvas"
-            className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Demo
-          </a>
-          <a
-            href="#tutorials"
-            className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Tutorials
           </a>
           <a
             href="#pricing"
